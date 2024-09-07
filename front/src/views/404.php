e594a4f898ecbbafcf01afff01824777dfac9ec6
Alexandre Uzan <alexandreuzan9@gmail.com>
2024-09-07 12:34:44 +0200

    reorganisation

--- front/src/views/404.php
+++ front/src/views/404.php
@@ -7,7 +7,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/views/includes/lang.php');
 <head>
     <meta charset="UTF-8">
     <title>404</title>
-    <link rel="stylesheet" href="/front/CSS/styles.css">
+    <link rel="stylesheet" href="/front/CSS/404.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHq6N2PRSDjH4rLWTv/f8K68jjg5mZDA5tNEgHf54RBIBp0/5WnZv7g0j7NJn/w3OqAtM2sw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
