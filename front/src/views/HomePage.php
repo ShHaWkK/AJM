82f86f147ec5abc1bcd93ba14043d6fbf408e3c1
Alexandre Uzan <alexandreuzan9@gmail.com>
2024-09-07 21:06:03 +0200

    Update HomePage

--- front/src/views/HomePage.php
+++ front/src/views/HomePage.php
@@ -8,11 +8,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $lang_data['title']; ?></title>
     <link rel="stylesheet" href="/assets/CSS/styles.css">
-</head>
-
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'); ?>
-
+    </head>
 <body>
+
+<?php require_once(__DIR__  . '/includes/header.php'); ?>
+
 <section class="hero">
     <div class="container">
         <h2><?php echo $lang_data['homepage']['title']; ?></h2>
