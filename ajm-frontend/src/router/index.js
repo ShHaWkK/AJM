cf4b65565a4bb13125d917325254f148292f5a26
Alexandre Uzan <alexandreuzan9@gmail.com>
2024-10-15 15:10:17 +0200

    Update router

--- ajm-frontend/src/router/index.js
+++ ajm-frontend/src/router/index.js
@@ -1,3 +1,4 @@
+// router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
 import QuoteFormPage from '../views/QuoteFormPage.vue';
@@ -5,6 +6,8 @@ import DashboardPage from '../views/DashboardPage.vue';
 import SupportTicketsPage from '../views/SupportTicketsPage.vue';
 import BlogPage from '../views/BlogPage.vue';
 import ContactPage from '../views/ContactPage.vue';
+import ServicesSectionPage from '../views/services/ServiceSectionPage.vue';
+import LoginInscriptionPage from '../views/Login_inscriPage.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
@@ -13,6 +16,8 @@ const routes = [
   { path: '/tickets', name: 'SupportTickets', component: SupportTicketsPage },
   { path: '/blog', name: 'Blog', component: BlogPage },
   { path: '/contact', name: 'Contact', component: ContactPage },
+  { path: '/services', name: 'Services', component: ServicesSectionPage },
+  { path: '/login', name: 'Login', component: LoginInscriptionPage },
 ];
 
 const router = createRouter({
