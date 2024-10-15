e2112af37350ca8b97bf388646ef7531d266cdd9
Alexandre Uzan <alexandreuzan9@gmail.com>
2024-10-15 17:27:23 +0200

    Update server.js

--- ajm-backend/server.js
+++ ajm-backend/server.js
@@ -1,8 +1,8 @@
-// app.js
 const express = require('express');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
-const connection = require('./config/db');
+const projectRoutes = require('./routes/projectRoutes');
+const connection = require('./config/database');
 
 dotenv.config();
 
@@ -12,6 +12,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api', projectRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
